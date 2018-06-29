a841f9038f0b8954cf500c69cd06678aad58fd04
ODIN-PC\ODIN <craigross.emailme@gmail.com>
2018-06-29 15:30:15 -0400

    Initial commit

---
+++ Assets/takeDammage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class takeDammage : MonoBehaviour {
+
+	public string dammageTag;
+	public GameObject screen;
+	public bool isStatic;
+	// Use this for initialization
+	void Start () {
+		if(isStatic == false)
+		{
+			transform.parent = screen.transform;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider hit)
+	{
+		if (hit.tag == dammageTag)
+		{
+			Destroy(hit.gameObject);
+			gameObject.GetComponent<enemyHealth>().TakeDamage(1);
+		}
+	}
+}
