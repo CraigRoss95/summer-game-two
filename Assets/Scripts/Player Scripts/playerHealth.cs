01142cbf2c7ca70771548169c1675a628b823969
craig's laptop <craigross.emailme@gmail.com>
2018-08-22 16:55:25 -0400

    dodge and i frame animations

--- Assets/Scripts/Player Scripts/playerHealth.cs
+++ Assets/Scripts/Player Scripts/playerHealth.cs
@@ -8,15 +8,21 @@ public int maxHealth;
 public int currHealth;
 private bool invinsable;
 public float iFrameTime;
+public GameObject playerModel;
+private bool visable;
+private bool animated;
+
 	// Use this for initialization
 	void Start () {
 		invinsable = false;
 		currHealth = maxHealth;
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		
 		if (currHealth <= 0)
 		{
 			gameObject.transform.parent.gameObject.SetActive(false);
@@ -26,10 +32,38 @@ public float iFrameTime;
 	}
 	public void TakeDamage(int damage)
 	{
-
+			
 			currHealth = currHealth - damage;
 			invinsable = true;
+			Flash();
 			Invoke("RemoveIFrames", iFrameTime);
+	}
+
+	void Flash()
+	{
+		if (invinsable == true)
+		{
+			if(visable == false)
+			{
+				playerModel.SetActive(true);
+				visable = true;
+				Invoke("Flash", 0.2f);
+				Debug.Log("Fric");
+			}
+			else
+			{
+				playerModel.SetActive(false);
+				visable = false;
+				Invoke("Flash", 0.2f);
+				Debug.Log("hec");
+			}
+		}
+		
+		else
+		{
+			playerModel.SetActive(true);
+				visable = true;
+		}
 	}
 
 	public bool GetInvinsable()
--- Assets/Scripts/dodge.cs
+++ Assets/Scripts/dodge.cs
@@ -8,32 +8,42 @@ private bool onCooldown;
 private bool dodgeing;
 public float dodgeTime;
 public float cooldownTime;
+public GameObject playerModel;
+public Material dodgeingTexture;
+private Material notDodgeingTexture;
 	// Use this for initialization
 	void Start () {
 		onCooldown = false;
 		dodgeing = false;
+		notDodgeingTexture = playerModel.GetComponent<Renderer>().material;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("dodge") && onCooldown == false && dodgeing == false && gameObject.GetComponent<playerHealth>().GetInvinsable() == true)
+		if (Input.GetButtonDown("dodge") && onCooldown == false && dodgeing == false && gameObject.GetComponent<playerHealth>().GetInvinsable() == false)
 		{
 			Dodge();
 		}
-		if(gameObject.GetComponent<playerHealth>().GetInvinsable() == true)
+		if(dodgeing == false)
 		{
-			gameObject.GetComponent<BoxCollider>().enabled = false;
+			if(gameObject.GetComponent<playerHealth>().GetInvinsable() == true)
+			{
+				gameObject.GetComponent<BoxCollider>().enabled = false;
+			}
+			else
+			{
+				gameObject.GetComponent<BoxCollider>().enabled = true;
+			}
 		}
-		else{
-			gameObject.GetComponent<BoxCollider>().enabled = true;
-		}
+		
 	}
 
 void Dodge()
 	{
 		dodgeing = true;
-		
+		playerModel.GetComponent<Renderer>().material = dodgeingTexture;
+		gameObject.GetComponent<BoxCollider>().enabled = false;
 		Invoke("UnDodge", dodgeTime);
 	}
 void UnDodge()
@@ -41,6 +51,9 @@ void UnDodge()
 		dodgeing = false;
 		gameObject.GetComponent<BoxCollider>().enabled = true;
 		onCooldown = true;
+		playerModel.GetComponent<Renderer>().material = notDodgeingTexture;
+		
+		
 		Invoke ("Cooldown", cooldownTime);
 
 	}
